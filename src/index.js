f95c5c41cc03402591b191d30b59a2e448da2d57
Dallair220 <93786532+Dallair220@users.noreply.github.com>
2023-04-18 22:58:43 +0200

    Finish for day

--- src/index.js
+++ src/index.js
@@ -13,15 +13,20 @@ const t1 = task('Task 1', 'Description', todayDate);
 const t2 = task('Task 2', 'Description', todayDate);
 const t3 = task('Task 3', 'Description', todayDate);
 const p2 = project('Default2');
+const t4 = task('TASK4', 'DESCRIPTION', todayDate);
 
 p1.addTask(t1);
 p1.addTask(t2);
 p1.addTask(t3);
-
 p1.removeTask(t3);
+p2.addTask(t4);
 
 const projectList = [p1, p2];
 projectSidebarLoader(projectList);
 
 console.log(t1);
 contentTaskLoader(p1);
+
+// projectList.forEach(project);
+
+// TODO: DEPENDING ON WHICH PROJECT IS SELECTED IN UI, A DIFFERENT TASKLIST SHOULD BE SHOWN IN CONTENT AREA
