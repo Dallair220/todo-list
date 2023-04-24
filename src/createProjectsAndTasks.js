dca9b65d1b1708953ac115d8939d3291d85f1d30
Dallair220 <93786532+Dallair220@users.noreply.github.com>
2023-04-24 13:12:13 +0200

    Add prompts as button events

--- src/createProjectsAndTasks.js
+++ src/createProjectsAndTasks.js
@@ -1,13 +1,31 @@
-function eventCall(e) {
-  console.log(e);
+import project from './project';
+import task from './task';
+
+function eventCallProject() {
+  const projectName = prompt('Please enter the project name');
+  const p = project(projectName);
+  console.log(p);
 }
 
-function domStuff() {
+function eventCallTask() {
+  const taskName = prompt('Please enter the task name');
+  const t = task(taskName);
+  console.log(t);
+}
+
+function projectButton() {
   document
     .querySelector('div.sidebar > button')
-    .addEventListener('click', eventCall);
+    .addEventListener('click', eventCallProject);
+}
+
+function taskButton() {
+  document
+    .querySelector('div.content > button')
+    .addEventListener('click', eventCallTask);
 }
 
 export default function loadEventListeners() {
-  domStuff();
+  projectButton();
+  taskButton();
 }
--- src/task.js
+++ src/task.js
@@ -1,5 +1,15 @@
+// eslint-disable-next-line import/no-extraneous-dependencies
+import { format } from 'date-fns';
+
+const todayDate = format(new Date(), 'dd/MM/yyyy');
+
 // Factory Function for new tasks
-export default function task(title, description, dueDate, isPriority = false) {
+export default function task(
+  title = 'default',
+  description = 'default',
+  dueDate = todayDate,
+  isPriority = false
+) {
   const isFinished = false;
 
   return {
